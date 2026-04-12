46a4c10797e78858c2c6faa127954783c7cf4b0a
Jan Esser <jesser@gmx.de>
2026-04-12 18:01:11 +0200

    set main-branch

--- git-submodules-pull.sh
+++ git-submodules-pull.sh
@@ -2,13 +2,15 @@
 
 pushd .
 cd $(dirname "$0")
+git checkout main
+git pull
 
 git config http.lowSpeedLimit 1000
 git config http.lowSpeedTime 600
 git submodule update --init --recursive
 
 cd meta-tegra
-git checkout -t origin/kirkstone-l4t-r32.7.x
+git checkout -t origin/kirkstone-l4t-r32.7.x || git checkout kirkstone-l4t-r32.7.x
 git pull
 
 #cd ../meta-tegra-community
@@ -16,11 +18,11 @@ git pull
 #git pull
 
 cd ../meta-openembedded
-git checkout -t origin/kirkstone
+git checkout -t origin/kirkstone || git checkout kirkstone
 git pull
 
 cd ../poky
-git checkout -t origin/kirkstone
+git checkout -t origin/kirkstone || git checkout kirkstone
 git pull
 
 popd
