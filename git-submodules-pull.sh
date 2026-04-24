1ae1541238bdd0d78a9879cb776b68bd1d2a40d5
Jan Esser <jesser@gmx.de>
2026-04-24 07:58:54 +0200

    low speed git hack

--- git-submodules-pull.sh
+++ git-submodules-pull.sh
@@ -3,6 +3,10 @@
 pushd .
 cd $(dirname "$0")
 
+git config http.lowSpeedLimit 1000
+git config http.lowSpeedTime 600
+git submodule update --init --recursive
+
 cd meta-tegra
 git checkout -t origin/kirkstone-l4t-r32.7.x
 git pull
